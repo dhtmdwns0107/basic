7db2f740d31f1f4297d70d72ff2a99b33b969e5c
Sean Oh <dhtmdwns0107@gmail.com>
2019-12-24 11:27:49 +0900

    191224SpringBoot

---
+++ basic/src/main/java/com/sean/basic/controller/LoginController.java
@@ -0,0 +1,31 @@
+package com.sean.basic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	@PostMapping("/login")
+	@ResponseBody
+	public String loginPost(
+			@RequestParam("id") String id,
+			@RequestParam("pw") String pw) {
+		
+		String dbId = "boot";
+		String dbPw = "1234";
+		
+		if(dbId.equals(id) && dbPw.equals(pw)) {
+			return "드루와";
+		}
+		
+		
+		return "나가";
+	}
+}
---
+++ basic/src/main/java/com/sean/basic/model/Member.java
@@ -0,0 +1,12 @@
+package com.sean.basic.model;
+
+import lombok.Data;
+
+@Data
+public class Member {
+	private String name;
+	private String userId;
+	private String userPassword;
+	
+}
+
